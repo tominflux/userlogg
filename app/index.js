2633cee1b8a58c734b0062714990f3d0a846ec03
Tom <tom.chesters96@gmail.com>
2020-08-09 18:17:45 +0100

    Test app

---
+++ app/index.js
@@ -0,0 +1,31 @@
+require('dotenv').config()
+
+const genUserloggMongoApi = require("../mongoApi/lib/api")
+const genUserloggApi = require("../lib/api")
+const genUserloggRestApi = require("../expressRestApi/lib/api")
+
+const userloggMongoApi = genUserloggMongoApi(
+    process.env.MONGO_CONNECTION,
+    process.env.MONGO_DATABASE
+)
+const userloggApi = genUserloggApi(userloggMongoApi)
+const userloggRestApi = genUserloggRestApi(userloggApi)
+
+
+//////////
+//////////
+
+
+const express = require('express')
+
+const app = express()
+const port = 3000
+
+app.use("/api", userloggRestApi)
+
+app.listen(
+    port,
+    () => console.log(
+        `Userlogg API listening at http://localhost:${port}`
+    )
+)
